62c08d169da934542fa419f863b8bfc64152b1f5
Uillian-ofical <uillian.gvalerio@gmail.com>
2020-10-26 14:09:56 -0300

    subindo exercicio api

---
+++ APIBoletim/Interfaces/IAluno.cs
@@ -0,0 +1,17 @@
+﻿using APIBoletim.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIBoletim.Interfaces
+{
+    interface IAluno
+    {
+        List<Aluno> ListarAlunos();
+        Aluno CadastrarAluno(Aluno _aluno);
+        Aluno BuscarAluno(int _idAluno);
+        Aluno AlterarAluno(Aluno _aluno, int _idAluno);
+        Aluno ExcluirAluno(int _idAluno);
+    }
+}
---
+++ APIBoletim/Repositories/AlunoRepository.cs
@@ -0,0 +1,42 @@
+﻿using APIBoletim.Domains;
+using APIBoletim.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIBoletim.Repositories
+{
+    public class AlunoRepository : IAluno
+    {
+        public Aluno AlterarAluno(Aluno _aluno, int _idAluno)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno BuscarAluno(int _idAluno)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno CadastrarAluno(Aluno _aluno)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno ExcluirAluno(int _idAluno)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Aluno> ListarAluno()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Aluno> ListarAlunos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
